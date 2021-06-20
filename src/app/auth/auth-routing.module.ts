1cd815b4293588d59c9eff35240ee60ea20e37d2
Sergio-Gual <sergiogual.istf@gmail.com>
2021-06-20 17:44:31 -0500

    lis

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { ProfileComponent } from './components/profile/profile.component';
+import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'profile', component: ProfileComponent },
+  { path: 'register', component: RegisterComponent},
 
 ];
 
