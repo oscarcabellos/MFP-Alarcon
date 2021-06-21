0446b75c6d983ebab13c9d82dd96859474e7c8d9
Alexander Marcos <marcos.valdez@hotmail.com>
2021-06-20 23:45:29 -0500

    solucion appService

--- src/app/core/appServiceBase.ts
+++ src/app/core/appServiceBase.ts
@@ -1,7 +1,17 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
 import { environment } from "src/environments/environment";
 
+@Injectable({
+    providedIn: 'root'
+  })
+
 export class AppServiceBase {
+
+    headers = new HttpHeaders()
+    .set('Content-Type', 'application/json')
+    .set('Cache-Control', 'no-cache');
 
     constructor(protected http: HttpClient){
     }
@@ -18,7 +28,7 @@ export class AppServiceBase {
 
     post(api:string, body: any){
         const urlApi=`${environment.api.baseUrl}${api}`;
-        return this.http.get(urlApi,body);
+        return this.http.post(urlApi,body,{headers: this.headers});
     }
 
     put(api: string, body: any) {
