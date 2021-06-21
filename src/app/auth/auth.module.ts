beabb69c861568fe12cd91bdf5269e8219b7b289
Eduardo <eduardo.navarro2@unmsm.edu.pe>
2021-06-21 00:59:40 -0500

    cambio en vistas

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -5,9 +5,10 @@ import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './components/login/login.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { RegisterComponent } from './components/register/register.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [LoginComponent, ProfileComponent, RegisterComponent],
-  imports: [CommonModule, AuthRoutingModule],
+  imports: [CommonModule, ReactiveFormsModule, FormsModule, AuthRoutingModule],
 })
 export class AuthModule {}
