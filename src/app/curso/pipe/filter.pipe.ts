97856a3b1aff1dabbcd33135515f259e0d246d53
Eduardo <eduardo.navarro2@unmsm.edu.pe>
2021-06-28 21:33:33 -0500

    refactor

--- src/app/curso/pipe/filter.pipe.ts
+++ src/app/curso/pipe/filter.pipe.ts
@@ -1,19 +1,23 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'filter'
+  name: 'filter',
 })
 export class FilterPipe implements PipeTransform {
-
   transform(items: any, filter: any, isAnd: boolean): any {
     if (filter && Array.isArray(items)) {
       const filterKeys = Object.keys(filter);
       if (isAnd) {
-        return items.filter(item =>
-          filterKeys.reduce((memo, keyName) =>
-            (memo && new RegExp(filter[keyName], 'gi').test(item[keyName])) || filter[keyName] === '', true));
+        return items.filter((item) =>
+          filterKeys.reduce(
+            (memo, keyName) =>
+              (memo && new RegExp(filter[keyName], 'gi').test(item[keyName])) ||
+              filter[keyName] === '',
+            true
+          )
+        );
       } else {
-        return items.filter(item => {
+        return items.filter((item) => {
           return filterKeys.some((keyName) => {
             if (filter[keyName]) {
               const fil = filter[keyName].split(' ');
@@ -37,5 +41,4 @@ export class FilterPipe implements PipeTransform {
       return items;
     }
   }
-
 }
