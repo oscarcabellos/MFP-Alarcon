086902fad05610c50bfe30669025811d2ddaa003
SebasRodz <sebasxiom@gmail.com>
2021-07-18 18:13:29 -0500

    Notificaciones V.1

---
+++ src/app/curso/servicios/notificaciones.service.ts
@@ -0,0 +1,15 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { AppServiceBase } from 'src/app/core/appServiceBase';
+import { Curso } from '../modelo/curso';
+
+@Injectable({
+    providedIn: 'root',
+  })
+  export class NotificacionService extends AppServiceBase {
+    listarCursosPublicos(): Observable<any> {
+        return this.get('coursespublic').pipe();                 
+      }
+  }
