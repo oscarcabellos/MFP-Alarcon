e34d7e30a2be7e41e4d7f30d6345255c46f904bb
Eduardo <eduardo.navarro2@unmsm.edu.pe>
2021-08-27 13:49:56 -0500

    docs: Se agrego comentarios al componente footer

--- src/app/layout/components/footer/footer.component.ts
+++ src/app/layout/components/footer/footer.component.ts
@@ -1,18 +1,31 @@
+/**
+ * Se importa las dependecias desde el core de angular
+ */
 import { Component, OnInit } from '@angular/core';
 
+/**
+ * Se declaran las dependencias con otros archivos
+ */
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
 })
+/**
+ * Se crea la clase para el componente footer
+ */
 export class FooterComponent implements OnInit {
-
+  /**
+   * Contructor del componente footer
+   */
   constructor() {
     //Codigo
-   }
+  }
 
+  /**
+   * Función que se llama al crear el componente
+   */
   ngOnInit(): void {
     // Codigo de inicializacion del componente
   }
-
 }
