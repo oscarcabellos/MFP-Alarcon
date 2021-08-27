346309e7289b5c4a8733a282e2ca4bdba9e5126a
Eduardo <eduardo.navarro2@unmsm.edu.pe>
2021-08-26 22:05:01 -0500

    docs: Se agrego comentarios al componente material-curso

--- src/app/curso/componente/material-curso/material-curso.component.spec.ts
+++ src/app/curso/componente/material-curso/material-curso.component.spec.ts
@@ -1,24 +1,59 @@
+/**
+ * Se importa los componente para la realización de las pruebas
+ */
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+/**
+ * Se importa el componente al cual se realizara las pruebas
+ */
 import { MaterialCursoComponent } from './material-curso.component';
 
+/**
+ * Se describe como se realizaran las pruebas
+ */
 describe('MaterialCursoComponent', () => {
+  /**
+   * Se crea un nuevo obtejo del material del curso
+   */
   let component: MaterialCursoComponent;
+
+  /**
+   * Se crea un objeto fixture relacionado con el material del curso
+   */
   let fixture: ComponentFixture<MaterialCursoComponent>;
 
+  /**
+   * Se hace la configuración para las pruebas del componente
+   */
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ MaterialCursoComponent ]
-    })
-    .compileComponents();
+      declarations: [MaterialCursoComponent],
+    }).compileComponents();
   });
 
+  /**
+   * Se inicializa los objetos dle componente
+   */
   beforeEach(() => {
+    /**
+     * Se incializa la creacion de un fixture
+     */
     fixture = TestBed.createComponent(MaterialCursoComponent);
+
+    /**
+     * Se incializa el componente para la realización de las pruebas
+     */
     component = fixture.componentInstance;
+
+    /**
+     * Se detecta los cambios del objeto fixture
+     */
     fixture.detectChanges();
   });
 
+  /**
+   * Se prueba la creación del compoennte
+   */
   it('should create', () => {
     expect(component).toBeTruthy();
   });
--- src/app/curso/componente/material-curso/material-curso.component.ts
+++ src/app/curso/componente/material-curso/material-curso.component.ts
@@ -1,32 +1,71 @@
+/**
+ * Se importa las librerias de angular
+ */
 import { Component, Input, OnInit } from '@angular/core';
+
+/**
+ * Se importa las dependencias para utilizar el modal
+ */
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
+/**
+ * Se importa el componente a redireccionar
+ */
 import { NuevoMaterialComponent } from '../nuevo-material/nuevo-material.component';
 
+/**
+ * Se configura los elementos que conforman el compoenente
+ */
 @Component({
   selector: 'app-material-curso',
   templateUrl: './material-curso.component.html',
   styleUrls: ['./material-curso.component.css'],
 })
+
+/**
+ * Clase para la creación del componente del material de un curso
+ */
 export class MaterialCursoComponent implements OnInit {
+  /**
+   * Parametro recibido desde el padre para validar si es usuario es el profesor del curso
+   */
   @Input() usuarioProfesor: boolean;
 
+  /**
+   * Array con la lista de los materiales del curso
+   */
   material = [];
+
+  /**
+   * Contructor para incializar el componente
+   * @param modalService Dependencias para la utilización del modal
+   */
   constructor(private modalService: NgbModal) {}
 
+  /**
+   * Función para incializar las variables necesarios del componente
+   */
   ngOnInit(): void {
     // Codigo de inicializacion del componente
   }
 
+  /**
+   * Función para abrir un modal con un nuevo material
+   */
   openModal() {
     const modalRef = this.modalService.open(NuevoMaterialComponent, {
       scrollable: true,
       windowClass: 'myCustomModalClass',
       size: 'lg',
     });
+    /**
+     * Información que se envia al componente padre
+     */
     let data = {
       tarea: false,
       editarTarea: false,
     };
+
     modalRef.componentInstance.fromParent = data;
     modalRef.result.then(
       (result) => {
