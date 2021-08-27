dd5f5f54cce802798f34e6d11229bed917cee70f
Eduardo <eduardo.navarro2@unmsm.edu.pe>
2021-08-26 20:38:44 -0500

    docs: Se agrego comentarios al componente editar-curso

--- src/app/curso/componente/editar-curso/editar-curso.component.spec.ts
+++ src/app/curso/componente/editar-curso/editar-curso.component.spec.ts
@@ -1,25 +1,66 @@
+/**
+ * Se importa las fucnioes desde el core de angular
+ */
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+/**
+ * Se importa el compoenente de editar curso para realizar las pruebas
+ */
 import { EditarCursoComponent } from './editar-curso.component';
 
+/**
+ * Se describe el conjunto de pruebas que se van a realizar
+ */
 describe('EditarCursoComponent', () => {
+  /**
+   * Se crea un objeto a partir del componente para editar un curso
+   */
   let component: EditarCursoComponent;
+
+  /**
+   * Se crea un objeto fixture con la referencia del componente de edición
+   */
   let fixture: ComponentFixture<EditarCursoComponent>;
 
+  /**
+   * Se crea una función que será ejecutada antes de cada prueba realizada
+   */
   beforeEach(async () => {
+    /**
+     * se incia el conjunto de pruebas con los modulos que se inicializarán
+     */
     await TestBed.configureTestingModule({
-      declarations: [ EditarCursoComponent ]
-    })
-    .compileComponents();
+      declarations: [EditarCursoComponent],
+    }).compileComponents();
   });
 
+  /**
+   * Función para iniciaa los objetos creados previamente
+   */
   beforeEach(() => {
+    /**
+     * Se instancia el conjunto de pruebas
+     */
     fixture = TestBed.createComponent(EditarCursoComponent);
+
+    /**
+     * Se instancia un componente de fixture
+     */
     component = fixture.componentInstance;
+
+    /**
+     * Se detectan los cambios del fixture
+     */
     fixture.detectChanges();
   });
 
+  /**
+   * Se comprueba la creación de la prueba
+   */
   it('should create', () => {
+    /**
+     * Se valida que el componente haya sido creado
+     */
     expect(component).toBeTruthy();
   });
 });
--- src/app/curso/componente/editar-curso/editar-curso.component.ts
+++ src/app/curso/componente/editar-curso/editar-curso.component.ts
@@ -1,19 +1,59 @@
+/**
+ * Se importa los modulos que se necesitan desde el core
+ */
 import { Component, Input, OnInit } from '@angular/core';
+
+/**
+ * Se importa el modulo del modal con sus funcionalidad
+ */
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+
+/**
+ * Se importa la clase Curso desde los modelos del módulo
+ */
 import { Curso } from '../../modelo/curso';
 
+/**
+ * Se crean las referencias a cada archivo que necesita el componente
+ */
 @Component({
   selector: 'app-editar-curso',
   templateUrl: './editar-curso.component.html',
   styleUrls: ['./editar-curso.component.css'],
 })
+
+/**
+ * Se crea la clase del componente de editar curso
+ */
 export class EditarCursoComponent implements OnInit {
+  /**
+   * Parametro recibido desde el componente padre
+   */
   @Input() fromParent;
+
+  /**
+   * Variable para determinar si se puede editar
+   */
   editar = true;
+
+  /**
+   * Objeto con la información dle curso
+   */
   curso: Curso;
+
+  /**
+   * Constructor con la inicialización de los modulos
+   * @param activeModal Modulo para el manejo del modal de angular
+   */
   constructor(public activeModal: NgbActiveModal) {}
 
+  /**
+   * Función para inicializar las variables
+   */
   ngOnInit(): void {
+    /**
+     * Se obtiene el curso desde el componente padre
+     */
     this.curso = this.fromParent.curso;
   }
 
@@ -22,6 +62,9 @@ export class EditarCursoComponent implements OnInit {
    * @param sendData Mensaje que se envia al componente padre
    */
   closeModal(sendData) {
+    /**
+     * Se envia mensaje al componente padre
+     */
     this.activeModal.close(sendData);
   }
 
@@ -30,6 +73,9 @@ export class EditarCursoComponent implements OnInit {
    * @param data mensaje que se recibe del componente hijo
    */
   closeModalChild(data: any) {
+    /**
+     * Se envia mensaje de cerrar al componete padre
+     */
     this.closeModal('cerrar');
   }
 }
