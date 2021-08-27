55058f650b50d7bf385414cdd887ddf0cc87b3a8
Eduardo <eduardo.navarro2@unmsm.edu.pe>
2021-08-26 23:02:53 -0500

    docs: Se agrego comentarios al componente ver-entrega-tarea

--- src/app/curso/componente/ver-entrega-tarea/ver-entrega-tarea.component.spec.ts
+++ src/app/curso/componente/ver-entrega-tarea/ver-entrega-tarea.component.spec.ts
@@ -1,27 +1,78 @@
+/**
+ * Se importa las librerias para realizar las pruebas
+ */
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+/**
+ * Se importa la libreria para el manejo de rutas
+ */
 import { Router } from '@angular/router';
+
+/**
+ * Se importa la lirbreria para el manejo dle modal
+ */
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
+/**
+ * Se importa el componente de ver entregas para realizar las pruebas
+ */
 import { VerEntregaTareaComponent } from './ver-entrega-tarea.component';
 
+/**
+ * Se describe el conjunto de pruebas que se realizaran
+ */
 describe('VerEntregaTareaComponent', () => {
+  /**
+   * Se crea un objeto del componente ver entrega
+   */
   let component: VerEntregaTareaComponent;
+
+  /**
+   * Se crea un objeto fixture
+   */
   let fixture: ComponentFixture<VerEntregaTareaComponent>;
 
+  /**
+   * Se inicializan las relaciones con otros modulos antes de cada prueba
+   */
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ VerEntregaTareaComponent ],
-      providers: [NgbActiveModal, { provide: Router, useClass: class { navigate = jasmine.createSpy("navigate"); }}]
-    })
-    .compileComponents();
+      declarations: [VerEntregaTareaComponent],
+      providers: [
+        NgbActiveModal,
+        {
+          provide: Router,
+          useClass: class {
+            navigate = jasmine.createSpy('navigate');
+          },
+        },
+      ],
+    }).compileComponents();
   });
 
+  /**
+   * Se inicializa las variables necesarias para las pruebas
+   */
   beforeEach(() => {
+    /**
+     * Se crea una instancia de la creación del componente
+     */
     fixture = TestBed.createComponent(VerEntregaTareaComponent);
+
+    /**
+     * Se crea una instancia del componente
+     */
     component = fixture.componentInstance;
+
+    /**
+     * Se detecta los cambios en el objeto fixture
+     */
     fixture.detectChanges();
   });
 
+  /**
+   * Se comprueba la creación del componente
+   */
   it('should create', () => {
     expect(component).toBeTruthy();
   });
--- src/app/curso/componente/ver-entrega-tarea/ver-entrega-tarea.component.ts
+++ src/app/curso/componente/ver-entrega-tarea/ver-entrega-tarea.component.ts
@@ -1,24 +1,64 @@
+/**
+ * Se importa las librerias para crear el componente
+ */
 import { Component, Input, OnInit } from '@angular/core';
+
+/**
+ * Se inporta las dependencias necesarias para utilizar el modal de bootstrap
+ */
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+
+/**
+ * Se importa la clase de entregar tarea
+ */
 import { EntregaTarea } from '../../modelo/entrega-tarea';
+
+/**
+ * Se importa el servicio para crear un nuevo material
+ */
 import { NuevoMaterialService } from '../../servicios/nuevo-material.service';
 
+/**
+ * Se añade los archvios necesarios para el componente
+ */
 @Component({
   selector: 'app-ver-entrega-tarea',
   templateUrl: './ver-entrega-tarea.component.html',
   styleUrls: ['./ver-entrega-tarea.component.css'],
 })
+
+/**
+ * Se crea la clase para el componente de ver entregas
+ */
 export class VerEntregaTareaComponent implements OnInit {
+  /**
+   * Parametros recibidos desde el componente padre
+   */
   @Input() fromParent;
+
+  /**
+   * array con lista de entregas realizadas
+   */
   tareas: EntregaTarea[] = [];
+
+  /**
+   * Constructor para inicializar las dependencias
+   * @param activeModal moduo para el manejo del modal
+   * @param materialService servicio para listar las entregas realizadas
+   */
   constructor(
     public activeModal: NgbActiveModal,
     private materialService: NuevoMaterialService
   ) {}
 
+  /**
+   * Función para incializar las variables del componente
+   */
   ngOnInit(): void {
     /* Codigo de ejecucion al inicio del componenente */
-
+    /**
+     * Se llama a la función para listar las tareas entregadas
+     */
     this.listarTareas(this.fromParent.tarea.tarea_id);
   }
 
@@ -27,13 +67,23 @@ export class VerEntregaTareaComponent implements OnInit {
    * @param sendData Mensaje que se envia la componente padre
    */
   closeModal(sendData) {
+    /**
+     * Se llama a la función para cerra el modal
+     */
     this.activeModal.close(sendData);
   }
 
+  /**
+   * Función para listar las entregas realizadas por los alumnos
+   * @param id Identificador del curso actual
+   */
   listarTareas(id) {
     this.materialService
       .listarEntregaTareas(id)
       .subscribe((x: EntregaTarea[]) => {
+        /**
+         * Se almacena la lista de tareas recibidas
+         */
         this.tareas = x;
       });
   }
