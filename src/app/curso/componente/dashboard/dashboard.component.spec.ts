d8374cc26e9de7057b03951b278c8ec8b46b88f6
Eduardo <eduardo.navarro2@unmsm.edu.pe>
2021-08-26 20:19:39 -0500

    docs: Se agrego comentarios al componente dashboard

--- src/app/curso/componente/dashboard/dashboard.component.spec.ts
+++ src/app/curso/componente/dashboard/dashboard.component.spec.ts
@@ -1,27 +1,79 @@
+/**
+ * Se importa los modulos para realizar las pruebas
+ */
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+/**
+ * Se importa el modulo de rutas estaticas
+ */
 import { Router } from '@angular/router';
 
+/**
+ * Se importa el componente para realizar las pruebas
+ */
 import { DashboardComponent } from './dashboard.component';
 
+/**
+ * Se comienza la descripción de las pruebas
+ */
 describe('DashboardComponent', () => {
+  /**
+   * Se crea un componente de la prueba
+   */
   let component: DashboardComponent;
+
+  /**
+   * se crear un fixture del componente
+   */
   let fixture: ComponentFixture<DashboardComponent>;
 
+  /**
+   * Se crea una funcion que sera llamada antes de la ejecución de cada prueba
+   */
   beforeEach(async () => {
+    /**
+     * Se importan los modulos necesarios para el componente
+     */
     await TestBed.configureTestingModule({
-      declarations: [ DashboardComponent ],
-      providers: [ { provide: Router, useClass: class { navigate = jasmine.createSpy("navigate"); } }]
-    })
-    .compileComponents();
+      declarations: [DashboardComponent],
+      providers: [
+        {
+          provide: Router,
+          useClass: class {
+            navigate = jasmine.createSpy('navigate');
+          },
+        },
+      ],
+    }).compileComponents();
   });
 
+  /**
+   * Se crea las instancias a utulizar durante las pruebas
+   */
   beforeEach(() => {
+    /**
+     * Se instancia un fixture del componente
+     */
     fixture = TestBed.createComponent(DashboardComponent);
+
+    /**
+     * Se crea una instancia del compoente dashboard
+     */
     component = fixture.componentInstance;
+
+    /**
+     * Se detectan los cambios en el fixture
+     */
     fixture.detectChanges();
   });
 
+  /**
+   * Se prueba la creacion del componente dashboard
+   */
   it('should create', () => {
+    /**
+     * Se valida la creación del componente
+     */
     expect(component).toBeTruthy();
   });
 });
--- src/app/curso/componente/dashboard/dashboard.component.ts
+++ src/app/curso/componente/dashboard/dashboard.component.ts
@@ -1,29 +1,80 @@
+/**
+ * Se importa las funciones de angular para poder incializar el componente
+ */
 import { Component, OnInit } from '@angular/core';
+
+/**
+ * Se importa el modulo de rutas para poder realizar el manejo a otras secciones
+ */
 import { Router } from '@angular/router';
 
+/**
+ * Se declara las variables que corresponden al componente
+ */
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css'],
 })
+
+/**
+ * Se crea la clase del componente dashboard con la implementación de la interface oninit
+ */
 export class DashboardComponent implements OnInit {
+  /**
+   * Variable para determinar si se accede para editar o para crear
+   */
   editar = false;
+
+  /**
+   * Constructor para incializar los modulos que se van a utilizar dentro del componente
+   * @param router Modulo para el manejo de las rutas dentro de la página
+   */
   constructor(private readonly router: Router) {}
 
+  /**
+   * Función que incializa las variables y hace llamadas a funciones
+   */
   ngOnInit(): void {
     // Codigo de inicializacion del componente
   }
 
   /**
-   * Función para cerrar sesión
+   * Función para cerrar sesión y redirigir hacia el componente de login
    */
   logout() {
+    /**
+     * Se elimina el identificador del usuario de las variables de sesión
+     */
     sessionStorage.removeItem('usuario_id');
+
+    /**
+     * Se elimina los apellidos del usuario de las variables de sesión
+     */
     sessionStorage.removeItem('usuario_apellidos');
+
+    /**
+     * Se elimina el nombre del usuario de las variables de sesión
+     */
     sessionStorage.removeItem('usuario_nombre');
+
+    /**
+     * Se elimina el correo del usuario de las variable de sesión
+     */
     sessionStorage.removeItem('correo');
+
+    /**
+     * Se elimina la url de la imagen de las variables de sesión
+     */
     sessionStorage.removeItem('url');
+
+    /**
+     * Se hace una redirección hacia la ruta correspondiente del login
+     */
     this.router.navigate(['user/login']).then(() => {
+      /**
+       * Se recarga la pagina una vez que se ha navegado hacia el login
+       */
       window.location.reload();
     });
   }
