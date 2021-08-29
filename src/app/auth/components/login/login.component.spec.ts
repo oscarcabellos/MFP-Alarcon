d9a66c8f4f162d81a570cdd70486055191e296a6
Sergio-Gual <sergiogual.istf@gmail.com>
2021-08-29 18:46:06 -0500

    com

--- src/app/auth/components/login/login.component.spec.ts
+++ src/app/auth/components/login/login.component.spec.ts
@@ -1,15 +1,50 @@
+/**
+ * Se importa el modulo de http para realizar consultas
+ */
+
 import { HttpClientModule } from '@angular/common/http';
+
+/**
+ * Se importa dos modulos para poder realizar las pruebas
+ */
+
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
+/**
+ * Se importa las funciones respectivas para realizar consultas hacia las rutas
+ */
 import { Router } from '@angular/router';
+
+/**
+ * Se importa el componente para realizar las pruebas
+ */
 
 import { LoginComponent } from './login.component';
 
+/**
+ * Se comienza las pruebas con un describe general
+ */
 describe('LoginComponent', () => {
+
+    /**
+   * Se instancia al componente al cual se hara pruebas
+   */
   let component: LoginComponent;
+
+    /**
+   * Se instancia un fixture que contenido al componente
+   */
   let fixture: ComponentFixture<LoginComponent>;
 
+    /**
+   * Se realiza la llamada a las funciones que se inicializaran antes de cada prueba
+   */
+
   beforeEach(async () => {
+
+    /**
+     * Se configura el testbed
+     */
     await TestBed.configureTestingModule({
       declarations: [ LoginComponent ],
       imports: [HttpClientModule, ReactiveFormsModule],
@@ -18,13 +53,36 @@ describe('LoginComponent', () => {
     .compileComponents();
   });
 
+  /**
+   * Se crea el componente antes de cada prueba
+   */
   beforeEach(() => {
+
+    /**
+     * Se asigna el contenido de las pruebas a fixture
+     */
     fixture = TestBed.createComponent(LoginComponent);
+
+    /**
+     * Se instancia un componente de la configuracion anterior
+     */
     component = fixture.componentInstance;
+
+    /**
+     * Se detectan los cambios realizados
+     */
     fixture.detectChanges();
   });
 
+
+  /**
+   * Prueba para comprobar la creación del componente
+   */
   it('should create', () => {
+
+    /**
+     * Comprobacion si esl componente se ha creado
+     */ 
     expect(component).toBeTruthy();
   });
 });
--- src/app/auth/components/register/register.component.spec.ts
+++ src/app/auth/components/register/register.component.spec.ts
@@ -1,15 +1,46 @@
+/**
+ * Se importa el modulo de http para realizar consultas
+ */
 import { HttpClientModule } from '@angular/common/http';
+
+/**
+ * Se importa dos modulos para poder realizar las pruebas
+ */
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
+
+/**
+ * Se importa el modulo de rutas estaticas
+ */
 import { Router } from '@angular/router';
+
+/**
+ * Se importa el componente para realizar las pruebas
+ */
 
 import { RegisterComponent } from './register.component';
 
+/**
+ * Se comienza las pruebas con un describe general
+ */
+
 describe('RegisterComponent', () => {
+  /**
+   * Se instancia al componente al cual se hara pruebas
+   */
   let component: RegisterComponent;
+  /**
+   * Se instancia un fixture que contenido al componente
+   */
   let fixture: ComponentFixture<RegisterComponent>;
 
+  /**
+   * Se realiza la llamada a las funciones que se inicializaran antes de cada prueba
+   */
   beforeEach(async () => {
+    /**
+     * Se configura el testbed
+     */
     await TestBed.configureTestingModule({
       declarations: [ RegisterComponent ],
       imports: [HttpClientModule, ReactiveFormsModule],
@@ -18,13 +49,37 @@ describe('RegisterComponent', () => {
     .compileComponents();
   });
 
+
+  /**
+   * Se crea el componente antes de cada prueba
+   */
   beforeEach(() => {
+
+    /**
+     * Se asigna el contenido de las pruebas a fixture
+     */
     fixture = TestBed.createComponent(RegisterComponent);
+
+    /**
+     * Se instancia un componente de la configuracion anterior
+     */
     component = fixture.componentInstance;
+
+    /**
+     * Se detectan los cambios realizados
+     */
     fixture.detectChanges();
   });
 
+
+  /**
+   * Prueba para comprobar la creación del componente
+   */
   it('should create', () => {
+
+    /**
+     * Comprobacion si esl componente se ha creado
+     */
     expect(component).toBeTruthy();
   });
 });
