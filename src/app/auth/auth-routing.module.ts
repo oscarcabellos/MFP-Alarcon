a5e19dcd68a01aca1cc7dd3b0969836ab4741de4
Juan Tirado <juan.tirado@unmsm.edu.pe>
2021-08-29 18:23:40 -0500

    auth y core comentarios

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,9 +1,31 @@
+ /**
+     * se importo NgModule
+     */
 import { NgModule } from '@angular/core';
+
+/**
+     * se importo RouterModule y Routes
+     */
 import { RouterModule, Routes } from '@angular/router';
+
+/**
+     * se importo LoginComponent
+     */
 import { LoginComponent } from './components/login/login.component';
+
+/**
+     * se importo ProfileComponent
+     */
 import { ProfileComponent } from './components/profile/profile.component';
+
+/**
+     * se importo RegisterComponent
+     */
 import { RegisterComponent } from './components/register/register.component';
 
+ /**
+     * se establecio las rutas
+     */
 const routes: Routes = [
   { path: '', redirectTo: 'login' },
   { path: 'login', component: LoginComponent },
@@ -11,8 +33,15 @@ const routes: Routes = [
   { path: 'register', component: RegisterComponent },
 ];
 
+/**
+     * se importo y exporto
+     */
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
+
+/**
+     * Exportando
+     */
 export class AuthRoutingModule {}
--- src/app/auth/modelos/usuario.ts
+++ src/app/auth/modelos/usuario.ts
@@ -1,7 +1,30 @@
+/**
+     * Modelo usuario
+     */
 export class Usuario {
+   
+  /**
+     * Se exporto usuario_nombre
+     */
   usuario_nombre: string;
+
+   /**
+     * Se exporto usuario_apellidos
+     */
   usuario_apellidos: string;
+
+   /**
+     * Se exporto correo
+     */
   correo: string;
+
+   /**
+     * Se exporto password
+     */
   password: string;
+
+   /**
+     * Se exporto url
+     */
   url: string;
 }
