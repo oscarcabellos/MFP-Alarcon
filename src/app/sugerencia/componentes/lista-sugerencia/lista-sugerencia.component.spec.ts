6df55659863644f2b7ddf8d6714751dd7ee36433
Sergio-Gual <sergiogual.istf@gmail.com>
2021-08-30 13:33:12 -0500

    A

--- src/app/sugerencia/componentes/lista-sugerencia/lista-sugerencia.component.spec.ts
+++ src/app/sugerencia/componentes/lista-sugerencia/lista-sugerencia.component.spec.ts
@@ -1,18 +1,57 @@
+/**
+ * Se importa el modulo de http para realizar consultas
+ */
 import { HttpClientModule } from '@angular/common/http';
+
+/**
+ * Se importa dos modulos para poder realizar las pruebas
+ */
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+/**
+ * Se importa modulo del formulario Builder
+ */
 import { FormBuilder } from '@angular/forms';
 import { NgxPaginationModule } from 'ngx-pagination';
+
+/**
+ * Se importa el componente Categoria para la realización de las pruebas
+ */
 import { Categoria } from 'src/app/curso/modelo/categoria';
+/**
+ * Se importa el componente Usuario para la realización de las pruebas
+ */
 import { Usuario } from 'src/app/curso/modelo/usuario';
+/**
+ * Se importa el componente Sugerencia para la realización de las pruebas
+ */
 import { Sugerencia } from '../../modelos/sugerencia';
 
+/**
+ * Se importa el componente  para la realización de las pruebas
+ */
 import { ListaSugerenciaComponent } from './lista-sugerencia.component';
 
+/**
+ * Se crea la descripción del contexto de pruebas
+ */
 describe('ListaSugerenciaComponent', () => {
+    /**
+   * Se instancia al componente al cual se hara pruebas
+   */
   let component: ListaSugerenciaComponent;
+  /**
+   * Se instancia un fixture que contenido al componente
+   */
   let fixture: ComponentFixture<ListaSugerenciaComponent>;
 
+  /**
+   * Se realiza la llamada a las funciones que se inicializaran antes de cada prueba
+   */
   beforeEach(async () => {
+
+    /**
+     * Se configura el testbed
+     */
     await TestBed.configureTestingModule({
       declarations: [ListaSugerenciaComponent],
       imports: [HttpClientModule, NgxPaginationModule],
@@ -20,19 +59,46 @@ describe('ListaSugerenciaComponent', () => {
     }).compileComponents();
   });
 
+  /**
+   * Se crea el componente antes de cada prueba
+   */
   beforeEach(() => {
+    /**
+     * Se asigna el contenido de las pruebas a fixture
+     */
     fixture = TestBed.createComponent(ListaSugerenciaComponent);
+    /**
+     * Se instancia un componente de la configuracion anterior
+     */
     component = fixture.componentInstance;
+    /**
+     * Se detectan los cambios realizados
+     */
     fixture.detectChanges();
   });
 
+  /**
+   * Prueba para comprobar la creación del componente
+   */
   it('should create', () => {
+
+    /**
+     * Comprobacion si el componente se ha creado
+     */
     expect(component).toBeTruthy();
   });
+
+  /**
+   * Se comprueba si se abrio el modal de nueva sugerencia
+   */
 
   it('abrir modal de neva sugerencia', () => {
     component.openModal();
   });
+
+  /**
+   * Se comprueba si se obtuvo el nombre de la categoria
+   */
 
   it('obtener el nombre de la categoria', () => {
     let categoriaTest = new Categoria();
@@ -42,6 +108,10 @@ describe('ListaSugerenciaComponent', () => {
     expect(component.getNombreCategoria(1)).toEqual('uno');
   });
 
+
+  /**
+   * Se comprueba si se obtuvo el nombre de la cateogira indefinida
+   */
   it('obtener el nombre de la categoria undefined', () => {
     let categoriaTest = new Categoria();
     categoriaTest.categoria_nombre = 'uno';
@@ -52,18 +122,27 @@ describe('ListaSugerenciaComponent', () => {
     );
   });
 
+  /**
+   * Se comprueba el cambio de pagina
+   */
   it('cambiar de pagina', () => {
     component.pageActual = 10;
     component.cambiarPagina();
     expect(component.pageActual).toEqual(1);
   });
 
+  /**
+   * Se comprueba la actualizacion de la categoria
+   */
   it('actualizar categoria', () => {
     let categoriaTest = new Categoria();
     categoriaTest.categoria_id = 0;
     component.actualizarCategoria(categoriaTest);
   });
 
+  /**
+   * Se comprueba la actualizacion de la categoria
+   */
   it('actualizar categoria', () => {
     let categoriaTest = new Categoria();
     categoriaTest.categoria_id = 1;
@@ -73,11 +152,18 @@ describe('ListaSugerenciaComponent', () => {
     component.actualizarCategoria(categoriaTest);
   });
 
+  /**
+   * Se comprueba si se obtiene voto
+   */
   it('obtener voto', () => {
     let newUser = new Usuario();
     component.usuariosVotos = [newUser];
     component.obtenerVotoUsuario(1);
   });
+
+  /**
+   * Se comprueba la obtencion de la cantidad de votos indefinidos
+   */
 
   it('obtener cantidad de votos undefined', () => {
     component.sugerenciasVotos = [
@@ -86,6 +172,9 @@ describe('ListaSugerenciaComponent', () => {
     component.obtenerCantidadVotos(1);
   });
 
+  /**
+   * Se comprueba la obtencion de la cantidad de votos
+   */
   it('obtener cantidad de votos ', () => {
     component.sugerenciasVotos = [
       { 'COUNT(sugerencia_id)': 2, sugerencia_id: 1 },
@@ -93,15 +182,24 @@ describe('ListaSugerenciaComponent', () => {
     component.obtenerCantidadVotos(1);
   });
 
+  /**
+   * Se comprueba el listado de votos por usuario
+   */
   it('listar votos usuario', () => {
     component.listarVotosUsuarios(5);
   });
 
+  /**
+   * Se comprueba la creacion de componente
+   */
   it('crear componente', () => {
     sessionStorage.setItem('usuario_id', '5');
     component = fixture.componentInstance;
   });
 
+  /**
+   * Se comprueba el cambio de estado
+   */
   it('cambiar estado', () => {
     component.cambiarEstado(5);
   });
--- src/app/sugerencia/componentes/nueva-sugerencia/nueva-sugerencia.component.spec.ts
+++ src/app/sugerencia/componentes/nueva-sugerencia/nueva-sugerencia.component.spec.ts
@@ -1,15 +1,48 @@
+/**
+ * Se importa el modulo de http para realizar consultas
+ */
 import { HttpClientModule } from '@angular/common/http';
+
+/**
+ * Se importa dos modulos para poder realizar las pruebas
+ */
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+/**
+ * Se importa modulo del formulario Builder
+ */
 import { FormBuilder } from '@angular/forms';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
+/**
+ * Se importa el componente para realizar las pruebas
+ */
 import { NuevaSugerenciaComponent } from './nueva-sugerencia.component';
 
+/**
+ * Se comienza las pruebas con un describe general
+ */
+
 describe('NuevaSugerenciaComponent', () => {
+
+  /**
+   * Se instancia al componente al cual se hara pruebas
+   */
   let component: NuevaSugerenciaComponent;
+
+  /**
+   * Se instancia un fixture que contenido al componente
+   */
   let fixture: ComponentFixture<NuevaSugerenciaComponent>;
+  /**
+   * Se realiza la llamada a las funciones que se inicializaran antes de cada prueba
+   */
 
   beforeEach(async () => {
+
+    /**
+     * Se configura el testbed
+     */
     await TestBed.configureTestingModule({
       imports: [HttpClientModule],
       declarations: [NuevaSugerenciaComponent],
@@ -17,13 +50,34 @@ describe('NuevaSugerenciaComponent', () => {
     }).compileComponents();
   });
 
+  /**
+   * Se crea el componente antes de cada prueba
+   */
   beforeEach(() => {
+
+    /**
+     * Se asigna el contenido de las pruebas a fixture
+     */
     fixture = TestBed.createComponent(NuevaSugerenciaComponent);
+
+    /**
+     * Se instancia un componente de la configuracion anterior
+     */
     component = fixture.componentInstance;
+    /**
+     * Se detectan los cambios realizados
+     */
     fixture.detectChanges();
   });
 
+  /**
+   * Prueba para comprobar la creación del componente
+   */
   it('should create', () => {
+
+    /**
+     * Comprobacion si esl componente se ha creado
+     */
     expect(component).toBeTruthy();
   });
 });
